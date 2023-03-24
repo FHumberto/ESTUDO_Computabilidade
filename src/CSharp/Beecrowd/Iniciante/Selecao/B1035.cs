276360abcc9555ebd71e1687a9fbc02562647f76
Humberto Guedes <franciscohguedes@gmail.com>
2023-03-24 19:08:14 -0300

    [CSharp/Beecrowd] wip: problema 1035

---
+++ src/CSharp/Beecrowd/Iniciante/Selecao/B1035.cs
@@ -0,0 +1,11 @@
+﻿using Beecrowd.Template;
+
+namespace Beecrowd.Iniciante.Selecao;
+internal class B1035 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Teste de Seleção 1\n");
+
+    }
+}
