239e9b2ec472079e9603ba93a5d0c2b1d778504c
Humberto Guedes <franciscohguedes@gmail.com>
2023-03-28 11:42:39 -0300

    [CSharp/Beecrowd] wip: exercício B1045

---
+++ src/CSharp/Beecrowd/Iniciante/Selecao/B1045.cs
@@ -0,0 +1,11 @@
+﻿using Beecrowd.Template;
+
+namespace Beecrowd.Iniciante.Selecao;
+internal class B1045 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Tipos de Triângulos\n");
+
+    }
+}
