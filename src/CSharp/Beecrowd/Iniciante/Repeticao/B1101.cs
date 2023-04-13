d30ffda6a42215975fac6e0c149db91fb381d2be
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-13 07:53:44 -0300

    [CSharp/Beecrowd] wip: exercício B1101

---
+++ src/CSharp/Beecrowd/Iniciante/Repeticao/B1101.cs
@@ -0,0 +1,24 @@
+﻿using Beecrowd.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd.Iniciante.Repeticao;
+internal class B1101 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Sequência de Números e Soma\n");
+
+        while (true)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+            int m = int.Parse(linha[0]);
+            int n = int.Parse(linha[1]);
+
+            if()
+        }
+    }
+}
