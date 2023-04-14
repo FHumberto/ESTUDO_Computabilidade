cae409ecf8e84f897f89e0757d57a67451b13cf3
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-14 17:06:10 -0300

    [CSharp/Beecrowd] wip: exercício B1051

---
+++ src/CSharp/Beecrowd/Iniciante/Selecao/B1051.cs
@@ -0,0 +1,36 @@
+﻿using Beecrowd.Template;
+using System.Globalization;
+
+namespace Beecrowd.Iniciante.Selecao;
+internal class B1051 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Imposto de renda\n");
+
+        double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double imposto = 0.0;
+
+        if (renda >= 0.00 && renda <= 2000.00)
+        {
+
+        }
+        else if (renda >= 2000.01 && renda <= 3000.00)
+        {
+
+        }
+        else if (renda > 4500.00)
+        {
+
+        }
+
+        if (imposto == 0)
+        {
+            Console.WriteLine("Isento");
+        }
+        else
+        {
+            Console.WriteLine($"R$ {imposto.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
