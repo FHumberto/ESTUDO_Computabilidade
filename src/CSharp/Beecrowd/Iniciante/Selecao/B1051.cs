7a46dba1241f6bfd9ac06313e56b81ec40d5913f
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-16 12:52:13 -0300

    [CSharp/Beecrowd] add: resolução do exercício B1051

--- src/CSharp/Beecrowd/Iniciante/Selecao/B1051.cs
+++ src/CSharp/Beecrowd/Iniciante/Selecao/B1051.cs
@@ -9,22 +9,25 @@ internal class B1051 : Problema
         Console.WriteLine($"B{problema} - Imposto de renda\n");
 
         double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-        double imposto = 0.0;
-
-        if (renda >= 0.00 && renda <= 2000.00)
+        double imposto;
+        if (renda <= 2000.0)
         {
-
+            imposto = 0.0;
         }
-        else if (renda >= 2000.01 && renda <= 3000.00)
+        else if (renda <= 3000.0)
         {
-
+            imposto = (renda - 2000.0) * 0.08;
         }
-        else if (renda > 4500.00)
+        else if (renda <= 4500.0)
         {
-
+            imposto = (renda - 3000.0) * 0.18 + 1000.0 * 0.08;
+        }
+        else
+        {
+            imposto = (renda - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
         }
 
-        if (imposto == 0)
+        if (imposto == 0.0)
         {
             Console.WriteLine("Isento");
         }
