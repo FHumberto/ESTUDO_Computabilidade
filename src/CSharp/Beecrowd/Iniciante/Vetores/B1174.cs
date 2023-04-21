a12b00cccc5f8ce79158f53538b9d7abd732fa71
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-21 14:48:51 -0300

    [CSharp/Beecrowd] add: resolução do exercício B1174

---
+++ src/CSharp/Beecrowd/Iniciante/Vetores/B1174.cs
@@ -0,0 +1,23 @@
+﻿using Beecrowd.Template;
+using System.Globalization;
+
+namespace Beecrowd.Iniciante.Vetores;
+internal class B1174 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Seleção em Vetor I\n");
+
+        double[] valores = new double[100];
+
+        for (int i = 0; i < valores.Length; i++)
+        {
+            valores[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (valores[i] <= 10)
+            {
+                Console.WriteLine($"A[{i}] = {valores[i].ToString("F1", CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
