8bf72ec9ca87c05e4834f47c75a115d71cbcb486
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-21 15:03:25 -0300

    [CSharp/Beecrowd] wip: exercício B1133

---
+++ src/CSharp/Beecrowd/Iniciante/Repeticao/B1133.cs
@@ -0,0 +1,24 @@
+﻿using Beecrowd.Template;
+
+namespace Beecrowd.Iniciante.Repeticao;
+internal class B1133 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Múltiplos de 13\n");
+
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        int menor = Math.Min(x, y);
+        int maior = Math.Max(x, y);
+
+        for (int i = menor; i < maior; i++)
+        {
+            if (i % 5 == 2 || i % 5 == 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
