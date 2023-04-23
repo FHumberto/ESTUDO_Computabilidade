658c103b00191c9d68f425c31b48df30c2472a7c
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-22 22:11:41 -0300

    [CSharp/Beecrowd] add: resolução do exercício B1154

---
+++ src/CSharp/Beecrowd/Iniciante/Repeticao/B1154.cs
@@ -0,0 +1,32 @@
+﻿using Beecrowd.Template;
+using System.Globalization;
+
+namespace Beecrowd.Iniciante.Repeticao;
+internal class B1154 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Idades\n");
+
+        int numeros = 0;
+        int contador = 0;
+
+        while (true)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 0)
+            {
+                contador++;
+                numeros += numero;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        double media = (double)numeros / contador;
+        Console.WriteLine($"{media.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
