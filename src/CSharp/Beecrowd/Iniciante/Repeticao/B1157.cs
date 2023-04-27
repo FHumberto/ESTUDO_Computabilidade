4874bf03b173bdd16a8991e4e4d4388849f5fbb1
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-27 07:38:27 -0300

    [CSharp/Beecrowd] add: resolução do exercício B1157

---
+++ src/CSharp/Beecrowd/Iniciante/Repeticao/B1157.cs
@@ -0,0 +1,25 @@
+﻿using Beecrowd.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd.Iniciante.Repeticao;
+internal class B1157 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Divisores I\n");
+
+        int valor = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= valor; i++)
+        {
+            if(valor % i == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
