7b96f747a0f1108107ff2dbc9ff8d2979a2f1c55
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-28 13:59:27 -0300

    [Csharp/Beecrowd] add: resolução do exercício B1178

---
+++ src/CSharp/Beecrowd/Iniciante/Vetores/B1178.cs
@@ -0,0 +1,21 @@
+﻿using Beecrowd.Template;
+using System.Globalization;
+
+namespace Beecrowd.Iniciante.Vetores;
+internal class B1178 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Preenchimento de Vetor III\n");
+
+        decimal entrada = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        decimal[] vetor = new decimal[100];
+        vetor[0] = entrada;
+        Console.WriteLine($"N[0] = {vetor[0].ToString("F4", CultureInfo.InvariantCulture)}");
+        for (int i = 1; i < 100; i++)
+        {
+            vetor[i] = vetor[i - 1] / 2;
+            Console.WriteLine($"N[{i}] = {Math.Round(vetor[i], 4, MidpointRounding.ToEven).ToString(CultureInfo.InvariantCulture)}");
+        }
+    }
+}
