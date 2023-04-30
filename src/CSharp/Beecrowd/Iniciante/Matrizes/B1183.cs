0389d7adfafc260bae8a697efd86fa91a000ebb3
Humberto Guedes <franciscohguedes@gmail.com>
2023-04-29 22:41:34 -0300

    [Csharp/Beecrowd] wip: exercício B1179

---
+++ src/CSharp/Beecrowd/Iniciante/Matrizes/B1183.cs
@@ -0,0 +1,27 @@
+﻿using Beecrowd.Template;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd.Iniciante.Matrizes;
+internal class B1183 : Problema
+{
+    public override void PMain(string problema)
+    {
+        Console.WriteLine($"B{problema} - Acima da Diagonal Principal\n");
+
+        double[,] matriz = new double[12, 12];
+        char entrada = char.Parse(Console.ReadLine());
+
+        for(int i = 0; i < 12; i++)
+        {
+            for(int j= 0; j < 12; j++)
+            {
+                matriz[i, j] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
---
+++ src/CSharp/Beecrowd/Iniciante/Vetores/B1179.cs
@@ -0,0 +1,51 @@
+﻿using Beecrowd.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd.Iniciante.Vetores;
+internal class B1179 : Problema
+{
+    public override void PMain(string problema)
+    {
+        int[] pares = new int[5];
+        int[] impares = new int[5];
+        int contPares = 0;
+        int contImpares = 0;
+
+        for (int i = 0; i < 15; i++)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            if(numero % 2 == 0)
+            {
+                pares[i] = numero;
+                contPares++;
+            }
+            else
+            {
+                impares[i] = numero;
+                contImpares++;
+            }
+
+            if(contPares > pares.Length)
+            {
+                foreach(var numeros in pares)
+                {
+                    Console.WriteLine(numeros);
+                }
+                contPares = 0;
+            }
+            else
+            {
+                foreach (var numeros in pares)
+                {
+                    Console.WriteLine(numeros);
+                }
+                contImpares = 0;
+            }
+        }
+    }
+}
